50d21ac1caef80945752d70ca047670eed32e429
gavrilov_v <gavrilov_v92@mail.ru>
2022-02-16 01:37:31 +0500

    Part2_lesson8

--- spring-web/recommendation-service/src/main/java/com/geekbrains/spring/web/recommendation/controllers/ProductsController.java
+++ spring-web/recommendation-service/src/main/java/com/geekbrains/spring/web/recommendation/controllers/ProductsController.java
@@ -1,13 +1,27 @@
 package com.geekbrains.spring.web.recommendation.controllers;
 
 
+import com.geekbrains.spring.web.api.core.ProductDto;
+import com.geekbrains.spring.web.recommendation.services.StatisticService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductsController {
+    private final StatisticService statisticService;
 
+    @GetMapping("/purchased")
+    public List<ProductDto>getFrequentlyPurchasedProducts(){
+        return statisticService.getRecommendationThreeProducts();
+    }
+
+    @GetMapping("/folding")
+    public List<ProductDto>getFrequentlyFoldingToCartProducts(){
+        return statisticService.getAddToCartThreeProducts();
+    }
 
 }
